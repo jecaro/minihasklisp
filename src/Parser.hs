91a72555fcdb492e9b77306a59fb6048d463b23c
jecaro <jeancharles.quillet@gmail.com>
2021-09-17 22:36:25 +0200

    add exercise 2.1

--- src/Parser.hs
+++ src/Parser.hs
@@ -2,72 +2,105 @@ module Parser where
 
 import Text.Read (readMaybe)
 
-type Parser a = String -> Maybe (a, String)
+newtype Parser a = Parser
+    { runParser :: String -> Maybe (a, String)
+    }
 
 parseChar :: Char -> Parser Char
-parseChar _ [] = Nothing
-parseChar c (x : xs)
-    | c == x = Just (x, xs)
-    | otherwise = Nothing
+parseChar = Parser . parseCharF
+  where
+    parseCharF _ [] = Nothing
+    parseCharF c (x : xs)
+        | c == x = Just (x, xs)
+        | otherwise = Nothing
 
 parseAnyChar :: String -> Parser Char
-parseAnyChar [] _ = Nothing
-parseAnyChar (x : xs) str
-    | Just r <- parseChar x str = Just r
-    | otherwise = parseAnyChar xs str
+parseAnyChar = Parser . parseAnyCharF
+  where
+    parseAnyCharF [] _ = Nothing
+    parseAnyCharF (x : xs) str
+        | Just r <- runParser (parseChar x) str = Just r
+        | otherwise = parseAnyCharF xs str
 
 parseOr :: Parser a -> Parser a -> Parser a
-parseOr p1 p2 str
-    | Just r <- p1 str = Just r
-    | otherwise = p2 str
+parseOr (Parser f1) (Parser f2) = Parser parseOrF
+  where
+    parseOrF str
+        | Just r <- f1 str = Just r
+        | otherwise = f2 str
 
 parseAnyChar' :: String -> Parser Char
-parseAnyChar' chars = foldr parseOr (const Nothing) parsers
+parseAnyChar' chars = foldr parseOr (Parser $ const Nothing) parsers
   where
     parsers = parseChar <$> chars
 
 parseAnd :: Parser a -> Parser b -> Parser (a, b)
-parseAnd p1 p2 str
-    | Just (r1, str1) <- p1 str = case p2 str1 of
-        Nothing -> Nothing
-        Just (r2, str2) -> Just ((r1, r2), str2)
-    | otherwise = Nothing
+parseAnd (Parser f1) (Parser f2) = Parser parseAndF
+  where
+    parseAndF str
+        | Just (r1, str1) <- f1 str = case f2 str1 of
+            Nothing -> Nothing
+            Just (r2, str2) -> Just ((r1, r2), str2)
+        | otherwise = Nothing
 
 parseMany :: Parser a -> Parser [a]
-parseMany p str
-    | (Just (r1, str1)) <- p str = case parseMany p str1 of
-        Nothing -> Just ([r1], str1)
-        Just (r2, str2) -> Just (r1 : r2, str2)
-    | otherwise = Just ([], str)
+parseMany = Parser . parseManyF . runParser
+  where
+    parseManyF f str
+        | (Just (r1, str1)) <- f str = case parseManyF f str1 of
+            Nothing -> Just ([r1], str1)
+            Just (r2, str2) -> Just (r1 : r2, str2)
+        | otherwise = Just ([], str)
 
 parseSome :: Parser a -> Parser [a]
-parseSome p str
-    | Just ((r1, r2), str') <- parseAnd p (parseMany p) str = Just (r1 : r2, str')
-    | otherwise = Nothing
+parseSome = Parser . parseSomeF
+  where
+    parseSomeF p str
+        | Just ((r1, r2), str') <- runParser (parseAnd p (parseMany p)) str =
+            Just (r1 : r2, str')
+        | otherwise = Nothing
 
 parseUInt :: Parser Int
-parseUInt str
-    | Just (intStr, str') <- parseSome (parseAnyChar ['0' .. '9']) str =
-        case readMaybe intStr of
-            Nothing -> Nothing
-            Just int -> Just (int, str')
-    | otherwise = Nothing
+parseUInt = Parser parseUIntF
+  where
+    parseUIntF str
+        | Just (intStr, str') <-
+            runParser (parseSome (parseAnyChar ['0' .. '9'])) str =
+            case readMaybe intStr of
+                Nothing -> Nothing
+                Just int -> Just (int, str')
+        | otherwise = Nothing
 
 parseInt :: Parser Int
-parseInt str
-    | Just (intStr, str') <- parseSome (parseAnyChar ('-' : ['0' .. '9'])) str =
-        case readMaybe intStr of
-            Nothing -> Nothing
-            Just int -> Just (int, str')
-    | otherwise = Nothing
+parseInt = Parser parseIntF
+  where
+    parseIntF str
+        | Just (intStr, str') <-
+            runParser
+                ( parseSome (parseAnyChar ('-' : ['0' .. '9']))
+                )
+                str =
+            case readMaybe intStr of
+                Nothing -> Nothing
+                Just int -> Just (int, str')
+        | otherwise = Nothing
 
 parseTuple :: Parser a -> Parser (a, a)
-parseTuple p str
-    | Just (((((_, a1), _), a2), _), str') <-
-        (parseOpen `parseAnd` p `parseAnd` parseComa `parseAnd` p `parseAnd` parseClose) str =
-        Just ((a1, a2), str')
-    | otherwise = Nothing
+parseTuple = Parser . parseTupleF
   where
-    parseOpen = parseChar '('
-    parseClose = parseChar ')'
-    parseComa = parseChar ','
+    parseTupleF p str
+        | Just (((((_, a1), _), a2), _), str') <-
+            runParser
+                ( parseOpen
+                    `parseAnd` p
+                    `parseAnd` parseComa
+                    `parseAnd` p
+                    `parseAnd` parseClose
+                )
+                str =
+            Just ((a1, a2), str')
+        | otherwise = Nothing
+      where
+        parseOpen = parseChar '('
+        parseClose = parseChar ')'
+        parseComa = parseChar ','
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,56 +6,56 @@ main :: IO ()
 main = hspec $ do
     describe "Parser" $ do
         it "parseChar" $ do
-            parseChar 'a' "abcd" `shouldBe` Just ('a', "bcd")
-            parseChar 'z' "abcd" `shouldBe` Nothing
-            parseChar 'a' "aaaa" `shouldBe` Just ('a', "aaa")
+            runParser (parseChar 'a') "abcd" `shouldBe` Just ('a', "bcd")
+            runParser (parseChar 'z') "abcd" `shouldBe` Nothing
+            runParser (parseChar 'a') "aaaa" `shouldBe` Just ('a', "aaa")
 
         it "parseAnyChar" $ do
-            parseAnyChar "bca" "abcd" `shouldBe` Just ('a', "bcd")
-            parseAnyChar "xyz" "abcd" `shouldBe` Nothing
-            parseAnyChar "bca" "cdef" `shouldBe` Just ('c', "def")
+            runParser (parseAnyChar "bca") "abcd" `shouldBe` Just ('a', "bcd")
+            runParser (parseAnyChar "xyz") "abcd" `shouldBe` Nothing
+            runParser (parseAnyChar "bca") "cdef" `shouldBe` Just ('c', "def")
 
         it "parseAnyChar'" $ do
-            parseAnyChar' "bca" "abcd" `shouldBe` Just ('a', "bcd")
-            parseAnyChar' "xyz" "abcd" `shouldBe` Nothing
-            parseAnyChar' "bca" "cdef" `shouldBe` Just ('c', "def")
+            runParser (parseAnyChar' "bca") "abcd" `shouldBe` Just ('a', "bcd")
+            runParser (parseAnyChar' "xyz") "abcd" `shouldBe` Nothing
+            runParser (parseAnyChar' "bca") "cdef" `shouldBe` Just ('c', "def")
 
         it "parseOr" $ do
-            parseOr (parseChar 'a') (parseChar 'b') "abcd"
+            runParser (parseOr (parseChar 'a') (parseChar 'b')) "abcd"
                 `shouldBe` Just ('a', "bcd")
-            parseOr (parseChar 'a') (parseChar 'b') "bcda"
+            runParser (parseOr (parseChar 'a') (parseChar 'b')) "bcda"
                 `shouldBe` Just ('b', "cda")
-            parseOr (parseChar 'a') (parseChar 'b') "xyz"
+            runParser (parseOr (parseChar 'a') (parseChar 'b')) "xyz"
                 `shouldBe` Nothing
 
         it "parseAnd" $ do
-            parseAnd (parseChar 'a') (parseChar 'b') "abcd"
+            runParser (parseAnd (parseChar 'a') (parseChar 'b')) "abcd"
                 `shouldBe` Just (('a', 'b'), "cd")
-            parseAnd (parseChar 'a') (parseChar 'b') "bcda"
+            runParser (parseAnd (parseChar 'a') (parseChar 'b')) "bcda"
                 `shouldBe` Nothing
-            parseAnd (parseChar 'a') (parseChar 'b') "acd"
+            runParser (parseAnd (parseChar 'a') (parseChar 'b')) "acd"
                 `shouldBe` Nothing
 
         it "parseMany" $ do
-            parseMany (parseChar ' ') "    foobar"
+            runParser (parseMany (parseChar ' ')) "    foobar"
                 `shouldBe` Just ("    ", "foobar")
-            parseMany (parseChar ' ') "foobar    "
+            runParser (parseMany (parseChar ' ')) "foobar    "
                 `shouldBe` Just ("", "foobar    ")
 
         it "parseSome" $ do
-            parseSome (parseAnyChar ['0' .. '9']) "42foobar"
+            runParser (parseSome (parseAnyChar ['0' .. '9'])) "42foobar"
                 `shouldBe` Just ("42", "foobar")
-            parseSome (parseAnyChar ['0' .. '9']) "foobar42"
+            runParser (parseSome (parseAnyChar ['0' .. '9'])) "foobar42"
                 `shouldBe` Nothing
 
         it "parseUInt" $ do
-            parseUInt "42foobar" `shouldBe` Just (42, "foobar")
-            parseUInt "-42foobar" `shouldBe` Nothing
+            runParser parseUInt "42foobar" `shouldBe` Just (42, "foobar")
+            runParser parseUInt "-42foobar" `shouldBe` Nothing
 
         it "parseInt" $ do
-            parseInt "42foobar" `shouldBe` Just (42, "foobar")
-            parseInt "-42foobar" `shouldBe` Just (-42, "foobar")
+            runParser parseInt "42foobar" `shouldBe` Just (42, "foobar")
+            runParser parseInt "-42foobar" `shouldBe` Just (-42, "foobar")
 
         it "parseTuple" $ do
-            parseTuple parseInt "(123,456) foo bar "
+            runParser (parseTuple parseInt) "(123,456) foo bar "
                 `shouldBe` Just ((123, 456), " foo bar ")
