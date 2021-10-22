b782afa3ac779593c6f0ac20ce8a809ace8d65be
jecaro <jeancharles.quillet@gmail.com>
2021-10-22 13:43:32 +0200

    put parseAndEval in a separate module

--- hal/Main.hs
+++ hal/Main.hs
@@ -1,14 +1,11 @@
-import Data.Bifunctor
 import Data.Maybe
-import Hal.Eval (Env, eval, renderError)
-import Hal.SExpr
-import Parser
+import Hal.Eval
+import Hal.ParseAndEval
 import System.Console.Haskeline
 import System.Environment
 
 import Control.Monad.IO.Class (liftIO)
 import Data.Either
-import qualified Hal.Eval as Eval
 
 data Options = Options
     { opFile :: Maybe String
@@ -64,28 +61,3 @@ repl e = evalLine =<< getInputLine "> "
   where
     evalLine Nothing = repl e
     evalLine (Just input) = parseEvalPrint input e >>= repl
-
-data Error = ErParse String | ErEval Eval.Error
-type Result = Either Error (SExprValue, Env)
-
-renderResult :: Result -> String
-renderResult (Left (ErEval err)) = renderError err
-renderResult (Left (ErParse input)) = "Unable to parse: " <> input
-renderResult (Right (v, e')) =
-    unlines
-        [ "r = " <> renderValue v
-        , "e = " <> show (second toPairsValue <$> e')
-        ]
-
-parseAndEval :: String -> Env -> Either Error (SExprValue, Env)
-parseAndEval input e = do
-    (s, input') <- withParseError $ runParser parseSExpr input
-    r@(_, e') <- withEvalError $ eval s e
-    case input' of
-        "" -> Right r
-        _ -> parseAndEval input' e'
-  where
-    withParseError Nothing = Left $ ErParse input
-    withParseError (Just r) = Right r
-    withEvalError (Left err) = Left $ ErEval err
-    withEvalError (Right x) = Right x
---
+++ src/Hal/ParseAndEval.hs
@@ -0,0 +1,33 @@
+module Hal.ParseAndEval where
+
+import Data.Bifunctor (second)
+import Hal.Eval hiding (Error)
+import Hal.SExpr
+import Parser
+
+import qualified Hal.Eval as Eval
+
+data Error = ErParse String | ErEval Eval.Error
+type Result = Either Error (SExprValue, Env)
+
+renderResult :: Result -> String
+renderResult (Left (ErEval err)) = renderError err
+renderResult (Left (ErParse input)) = "Unable to parse: " <> input
+renderResult (Right (v, e')) =
+    unlines
+        [ "r = " <> renderValue v
+        , "e = " <> show (second toPairsValue <$> e')
+        ]
+
+parseAndEval :: String -> Env -> Either Error (SExprValue, Env)
+parseAndEval input e = do
+    (s, input') <- withParseError $ runParser parseSExpr input
+    r@(_, e') <- withEvalError $ eval s e
+    case input' of
+        "" -> Right r
+        _ -> parseAndEval input' e'
+  where
+    withParseError Nothing = Left $ ErParse input
+    withParseError (Just r) = Right r
+    withEvalError (Left err) = Left $ ErEval err
+    withEvalError (Right x) = Right x
